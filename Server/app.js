6f30e780e094e56e950424aa8aa3307895a406e0
Nicolas <Phoenix-Left@hotmail.fr>
2014-12-04 18:53:33 +0100

    Add express.js

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,11 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<h1>Hello World</h1>');
+  res.end();
+});
+
+app.listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
